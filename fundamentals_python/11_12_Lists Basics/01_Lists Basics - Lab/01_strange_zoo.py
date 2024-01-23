862e215768c237cd227f7adc9f420654af58bb29
Nenogzar <stoyan.naidenov@gmail.com>
2024-01-23 22:43:57 +0200

    update code

--- fundamentals_python/11_12_Lists Basics/01_Lists Basics - Lab/01_strange_zoo.py
+++ fundamentals_python/11_12_Lists Basics/01_Lists Basics - Lab/01_strange_zoo.py
@@ -14,17 +14,18 @@ my_list[0], my_list[2] = my_list[2], my_list[0]
 print(my_list)
 
 ############################################*-*[::1]*-*#############################################
-my_list = []
+my_list1 = []
 
 for _ in range(3):
     data = input()
-    my_list.append(data)
-new_list = my_list[::-1]
+    my_list1.append(data)
+new_list = my_list1[::-1]
 print(new_list)
 ###########################################*-*revers[]*-*###########################################
-my_list = []
+my_list2 = []
 
 for _ in range(3):
     data = input()
-    my_list.append(data)
-print(new_list.reverse())
+    my_list2.append(data)
+    new_list2 = my_list2.reverse()
+print(new_list2)
