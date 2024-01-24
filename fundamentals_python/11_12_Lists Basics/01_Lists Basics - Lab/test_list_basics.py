bf3d609c28e743af7083d2f1bf49c85e4d6715e3
Nenogzar <stoyan.naidenov@gmail.com>
2024-01-24 21:51:16 +0200

    Update

--- fundamentals_python/11_12_Lists Basics/01_Lists Basics - Lab/test_list_basics.py
+++ fundamentals_python/11_12_Lists Basics/01_Lists Basics - Lab/test_list_basics.py
@@ -1,23 +1,15 @@
-n = int(input())
+num_lists = int(input("Въведете брой на списъците: "))
+min_values_list = []
+for i in range(num_lists):
+    input_list = [int(x) for x in input(f"Въведете списък от числа разделен с интервал.  Списък:{i + 1}: ").split()]
+                # input_list = list(map(int, input(f"Въведете списък от числа разделен с интервал.  Списък: {i + 1}: ").split()))
+    if not input_list:
+        print("Празен списък. Пропускане.")
+        continue
 
-for _ in range(n):
-    line = input()
-
-    name_start = line.index('@')
-    print("name_start ", name_start)
-    name_end = line.index('|', name_start)
-    print("name_end ", name_end)
-    age_start = line.index('#', name_end)
-    print("age_start ", age_start)
-    age_end = line.index('*', age_start)
-    print("age_end ", age_end)
-
-    if name_start != -1 and name_end != -1 and age_start != -1 and age_end != -1:
-        name = line[name_start + 1:name_end]
-        age = line[age_start + 1:age_end]
-        print(f"{name} is {age} years old.")
-
-"""""""""
-input
-1
-Here is a name @George| and an age #18*
+    # Намиране на минималната стойност в текущия списък и добавяне в новия лист
+    min_value = min(input_list)
+    min_values_list.append(min_value)
+max_item = max(min_values_list)
+# Извеждане на новия лист с минималните стойности
+print("Новият лист съдържа минималните стойности от въведените списъци:", min_values_list.remove(max_item))
