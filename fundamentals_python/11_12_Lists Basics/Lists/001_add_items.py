fa42e2b8d735cea616f5e33364543d77a67fce28
Nenogzar <stoyan.naidenov@gmail.com>
2024-01-27 19:12:56 +0200

    move list to SoftUni

---
+++ fundamentals_python/11_12_Lists Basics/Lists/001_add_items.py
@@ -0,0 +1,27 @@
+""" Add List Items """
+#########################################################
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+#########################################################
+""" Append """
+# Using the append() method to append an item:
+
+thislist.append("orange")
+# output: ['apple', 'banana', 'cherry', 'orange']
+
+thislist.append(tropical)
+# output: ['apple', 'banana', 'cherry', 'orange', ["mango", "pineapple", "papaya"]]
+
+#########################################################
+""" Insert """
+# The insert() method inserts an item at the specified index:
+
+thislist.insert(1, "orange")
+# output: ['apple', 'orange', 'banana', 'cherry', 'orange', ["mango", "pineapple", "papaya"]]
+#########################################################
+""" Extend """
+#To append elements from another list to the current list, use the extend() method.
+
+thislist.extend(tropical)
+# output: ['apple', 'banana', 'cherry', 'mango', 'pineapple', 'papaya']
+print(thislist)
