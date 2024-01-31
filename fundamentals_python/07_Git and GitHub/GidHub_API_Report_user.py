ee72cefdeb386c7d3b1703d8c5d2de8c8d00739b
Nenogzar <stoyan.naidenov@gmail.com>
2024-02-01 00:04:31 +0200

    GitHub_API

--- fundamentals_python/07_Git and GitHub/GidHub_API_Report_user.py
+++ fundamentals_python/07_Git and GitHub/GidHub_API_Report_user.py
@@ -1,8 +1,20 @@
-while True:
+import requests
 
-    # code
+def get_github_data(username):
+    url = f"https://api.github.com/users/{username}/repos"
+    response = requests.get(url)
 
-    repeat = input("Искате ли да повторите (Y/N)? ")
-    if repeat.upper() == "N":
-        print("Благодаря!")
-        break
+    if response.status_code == 200:
+        repositories = response.json()
+        for repo in repositories:
+            repo_name = repo["name"]
+            repo_language = repo["language"]
+            repo_stars = repo["stargazers_count"]
+            print(f"Repository: {repo_name}, Language: {repo_language}, Stars: {repo_stars}")
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
+
+# Заменете "YOUR_USERNAME" с вашето GitHub потребителско име
+github_username = input("Input GidHub name: ")
+
+get_github_data(github_username)
