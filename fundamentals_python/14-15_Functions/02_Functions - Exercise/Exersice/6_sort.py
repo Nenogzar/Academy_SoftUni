46e4bda7f6d04af439383a08783b88d618ef1eff
Nenogzar <stoyan.naidenov@gmail.com>
2024-02-04 02:39:34 +0200

    function_exercise

--- fundamentals_python/14-15_Functions/02_Functions - Exercise/Exersice/6_sort.py
+++ fundamentals_python/14-15_Functions/02_Functions - Exercise/Exersice/6_sort.py
@@ -3,15 +3,16 @@ new_list = list(map(int, input_str.split()))
 sorted_list = sorted(new_list)
 print(sorted_list)
 
-
-
-
+""" 2 """
 
 print(sorted(list(map(int, input().split()))))
+
+""" 3 """
 
 
 def convert_to_int_list(input_string):
     return sorted(list(map(int, input_string.split())))
+
 
 # Example usage:
 input_numbers = input()
