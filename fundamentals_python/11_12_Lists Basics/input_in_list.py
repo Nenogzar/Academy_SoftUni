8a55651c4fe248a4e2b94f761069d3c6e11d0087
Nenogzar <stoyan.naidenov@gmail.com>
2024-02-05 21:02:43 +0200

    Update

--- fundamentals_python/11_12_Lists Basics/input_in_list.py
+++ fundamentals_python/11_12_Lists Basics/input_in_list.py
@@ -1,25 +1,25 @@
-""" stre to int"""
+""" string to int"""
 str_list = []
-list_one = input().split(", ")
+list_one = input("1: ").split(", ")
 for num in list_one:
     number = int(num)
     str_list.append(number)
 print(f"{list_one = }")
 print(f"{str_list = }")
 """ 1 """
-list_two = list(map(int, input().strip().split(", ")))
+list_two = list(map(int, input("2: ").strip().split(", ")))
 print(f"{list_two = }")
 
 """ 2 """
-list_three = [int(number) for number in input().split(", ")]
+list_three = [int(number) for number in input("3: ").split(", ")]
 print(f"{list_three = }")
 
 """ Invert list """
-invert_list = [-int(number) for number in input().split(", ")]
+invert_list = [-int(number) for number in input("4: ").split(", ")]
 print(f"{invert_list = }")
 
 """ 3 """
-list_int = list(map(int, input().split(", ")))
+list_int = list(map(int, input("5: ").split(", ")))
 print(f"{list_int = }")
 
 
@@ -28,5 +28,5 @@ print(f"{list_int = }")
 
 letters = "AB"
 number_list = list(range(1, 11))
-combo_list = [f"{leter}-{number}" for leter in letters for number in number_list]
+combo_list = [f"{leter}-{number}" for leter in letters for number in list_three]
 print(f"{combo_list = }")
