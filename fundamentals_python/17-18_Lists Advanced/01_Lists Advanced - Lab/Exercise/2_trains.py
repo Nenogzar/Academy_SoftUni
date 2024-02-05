a4202f48edca160557e680958c34016aaf541e2c
Nenogzar <stoyan.naidenov@gmail.com>
2024-02-05 23:33:53 +0200

    Update

--- fundamentals_python/17-18_Lists Advanced/01_Lists Advanced - Lab/Exercise/2_trains.py
+++ fundamentals_python/17-18_Lists Advanced/01_Lists Advanced - Lab/Exercise/2_trains.py
@@ -1,60 +1,59 @@
-# wagon_number = int(input())
-# train_wagon_index = []
-#
-# for _ in range(wagon_number):
-#     train_wagon_index.append(0)
-# # print(train_wagon_index)
-#
-# command_list = input().split()
-#
-# while command_list[0] != "End":
-#
-#     if command_list[0].lower() == "add":
-#         train_wagon_index[-1] += int(command_list[1])
-#     elif command_list[0].lower() == "insert":
-#
-#         train_wagon_index[int(command_list[1])] += int(command_list[2])
-#     elif command_list[0].lower() == "leave":
-#         train_wagon_index[int(command_list[1])] -= int(command_list[2])
-#     command_list = input().split()
-#
+wagon_number = int(input())
+train_wagon_index = []
+
+for _ in range(wagon_number):
+    train_wagon_index.append(0)
 # print(train_wagon_index)
+
+command_list = input().split()
+
+while command_list[0] != "End":
+
+    if "add" in command_list:
+        train_wagon_index[-1] += int(command_list[1])
+    elif "insert" in command_list:
+        train_wagon_index[int(command_list[1])] += int(command_list[2])
+    elif "leave" in command_list:
+        train_wagon_index[int(command_list[1])] -= int(command_list[2])
+    command_list = input().split()
+
+print(train_wagon_index)
 
 
 """ 2 """
-train_wagons = int(input())
-train = list()
-command = input()
-
-for n in range(train_wagons):
-    train.append(0)
-
-
-def add_people(umber_people):
-    train[-1] += umber_people
-
-
-def insert_people(wagon, number_people):
-    train[wagon] += number_people
-
-
-def leave_people(wagon, number_people):
-    train[wagon] -= number_people
-
-
-while command != "End":
-
-    command = command.split()
-
-    if "add" in command:
-        add_people(int(command[-1]))
-
-    elif "insert" in command:
-        insert_people((int(command[1])), int(command[-1]))
-
-    elif "leave" in command:
-        leave_people(int(command[1]), int(command[-1]))
-
-    command = input()
-
-print(train)
+# train_wagons = int(input())
+# train = []
+# command = input()
+#
+# for n in range(train_wagons):
+#     train.append(0)
+#
+#
+# def add_people(umber_people):
+#     train[-1] += umber_people
+#
+#
+# def insert_people(wagon, number_people):
+#     train[wagon] += number_people
+#
+#
+# def leave_people(wagon, number_people):
+#     train[wagon] -= number_people
+#
+#
+# while command != "End":
+#
+#     command = command.split()
+#
+#     if "add" in command:
+#         add_people(int(command[-1]))
+#
+#     elif "insert" in command:
+#         insert_people((int(command[1])), int(command[-1]))
+#
+#     elif "leave" in command:
+#         leave_people(int(command[1]), int(command[-1]))
+#
+#     command = input()
+#
+# print(train)
