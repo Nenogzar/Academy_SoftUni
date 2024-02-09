b05a04b20906168bf30e465b18ebe4f814e5bb52
Nenogzar <stoyan.naidenov@gmail.com>
2024-02-09 23:55:12 +0200

    List advance README.md Update

--- fundamentals_python/17-18_Lists Advanced/02_Lists Advanced - Exercise/Exercise/7_group_of_10.py
+++ fundamentals_python/17-18_Lists Advanced/02_Lists Advanced - Exercise/Exercise/7_group_of_10.py
@@ -1,11 +1,22 @@
+# sequence = list(map(int, input().split(", ")))
+#
+#
+# for group_start in range(1,101,10):
+#     group_end = group_start + 9
+#     group_numbers = [num for num in sequence if group_start <= num <= group_end]
+#
+#     if not any(group_start <= num <= group_end + 9 for num in sequence):
+#         continue
+#
+#     print(f"Group of {group_end}'s: {group_numbers}")
+
+""" 1 """
+
 sequence = list(map(int, input().split(", ")))
 
+max_sequence = max(sequence)
 
-for group_start in range(0, 100, 10):
-    group_end = group_start + 10
+for group_start in range(1, max_sequence+1, 10):
+    group_end = group_start + 9
     group_numbers = [num for num in sequence if group_start <= num <= group_end]
-
-    if not any(group_start <= num <= group_end + 10 for num in sequence):
-        break
-
     print(f"Group of {group_end}'s: {group_numbers}")
