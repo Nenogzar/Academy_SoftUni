348adb18e82483e4b76837fdeb230926647876ac
Nenogzar <stoyan.naidenov@gmail.com>
2024-02-10 07:10:54 +0200

    List advance exercise

--- fundamentals_python/17-18_Lists Advanced/02_Lists Advanced - Exercise/Exercise/7_group_of_10.py
+++ fundamentals_python/17-18_Lists Advanced/02_Lists Advanced - Exercise/Exercise/7_group_of_10.py
@@ -1,15 +1,3 @@
-# sequence = list(map(int, input().split(", ")))
-#
-#
-# for group_start in range(1,101,10):
-#     group_end = group_start + 9
-#     group_numbers = [num for num in sequence if group_start <= num <= group_end]
-#
-#     if not any(group_start <= num <= group_end + 9 for num in sequence):
-#         continue
-#
-#     print(f"Group of {group_end}'s: {group_numbers}")
-
 """ 1 """
 
 sequence = list(map(int, input().split(", ")))
@@ -20,3 +8,25 @@ for group_start in range(1, max_sequence+1, 10):
     group_end = group_start + 9
     group_numbers = [num for num in sequence if group_start <= num <= group_end]
     print(f"Group of {group_end}'s: {group_numbers}")
+
+""" 2 -  Ivan Shopov """
+
+numbers = [int(number) for number in input().split(", ")]
+current_group = 10
+while numbers:
+    filtered_numbers_for_current_group = [number for number in numbers if number <= current_group]
+    print(f"Group of {current_group}'s: {filtered_numbers_for_current_group}")
+    current_group += 10
+    numbers = [number for number in numbers if number not in filtered_numbers_for_current_group]
+
+
+""" 3 """
+
+number_list = [int(n) for n in input().split(", ")]
+
+for n in range(1, 11):
+    check_list = list()
+    if len(number_list) != 0:
+        [check_list.append(i) for i in number_list if i <= (n * 10)]
+        [number_list.remove(o) for o in check_list]
+        print(f"Group of {n * 10}'s: {check_list}")
