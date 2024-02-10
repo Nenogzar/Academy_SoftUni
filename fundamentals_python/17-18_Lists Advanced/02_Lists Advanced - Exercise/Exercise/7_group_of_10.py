51624b29b46e5f88c5fa18060b1065d429ff3d49
Nenogzar <stoyan.naidenov@gmail.com>
2024-02-10 19:59:01 +0200

    List advance exercise

--- fundamentals_python/17-18_Lists Advanced/02_Lists Advanced - Exercise/Exercise/7_group_of_10.py
+++ fundamentals_python/17-18_Lists Advanced/02_Lists Advanced - Exercise/Exercise/7_group_of_10.py
@@ -34,3 +34,50 @@ for number in range(1, 10 + 1):
             numbers.remove(d)
 
         print(f"Group of {number * 10}'s: {check_numbers}")
+
+""" 4 """
+
+
+def ten_division(num):
+    group = (num // 10) + 1
+    if num % 10 == 0:  # 10 falls in the group of 10s according to the exercise
+        group -= 1
+    return group
+
+
+numbers_list = [int(i) for i in input().split(', ')]
+groups_list = []
+for elem in numbers_list:
+    if ten_division(elem) not in groups_list:
+        groups_list.append(ten_division(elem))
+groups_list.sort()
+
+index = 0
+for g in groups_list:
+    if index + 1 < len(groups_list):
+        if groups_list[index + 1] - g > 1 and index + 1 <= len(
+                groups_list):  # need to add the missing groups (where there are no numbers)
+            groups_list.insert(index + 1, g + 1)
+    index += 1
+    print(f"Group of {g}0's: {[x for x in numbers_list if ten_division(x) == g]}")
+
+
+""" 5 whir dictionary """
+list_numbers = [int(x) for x in input().split(", ")]
+sorted_groups = {}
+group = 0
+
+while True:
+    if not list_numbers:
+        break
+
+    if max(list_numbers) > group:
+        group += 10
+
+    sorted_groups[group] = [num for num in list_numbers if num <= group]
+    list_numbers = [x for x in list_numbers if x > group]
+
+for key, value in sorted_groups.items():
+    print(f"Group of {key}'s: {value}")
+
+""" 6 """
