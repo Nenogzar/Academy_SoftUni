ba844db64d0f581c0dd1ee32403b2ac673aded72
Nenogzar <stoyan.naidenov@gmail.com>
2024-02-23 07:41:32 +0200

    UPDATE

--- fundamentals_python/lectures/22-23_Objects and Classes/01_Objects and Classes - Lab/1_comment.py
+++ fundamentals_python/lectures/22-23_Objects and Classes/01_Objects and Classes - Lab/1_comment.py
@@ -4,7 +4,15 @@ class Comment:
         self.content = content
         self.likes = likes
 
-comment = Comment("user1", "I like this book")
+comment1 = Comment("user1", "I like this book")
 print(comment.username)
 print(comment.content)
 print(comment.likes)
+
+comment2 = Comment(username="user2", content=" I like this  ", likes= 6)
+comment3 = Comment( "user3", 'I dont like', 3)
+
+print(f" I'm a {comment3.username}! {comment3.content} book!  I giv him {comment3.likes} points")
+
+
+
