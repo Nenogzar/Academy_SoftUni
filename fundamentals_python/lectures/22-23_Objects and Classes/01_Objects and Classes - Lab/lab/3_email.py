52dfc8813d4bc4a4f7b60a7d179049b38fc072a2
Nenogzar <stoyan.naidenov@gmail.com>
2024-02-24 20:02:46 +0200

    classes

--- fundamentals_python/lectures/22-23_Objects and Classes/01_Objects and Classes - Lab/lab/3_email.py
+++ fundamentals_python/lectures/22-23_Objects and Classes/01_Objects and Classes - Lab/lab/3_email.py
@@ -31,6 +31,40 @@
 
 """ """
 
+# class Email:
+#     def __init__(self, sender, receiver, content):
+#         self.sender = sender
+#         self.receiver = receiver
+#         self.content = content
+#         self.is_sent = False
+#
+#     def send(self):
+#         self.is_sent = True
+#
+#     def get_info(self):
+#         return f"{self.sender} says to {self.receiver}: {self.content}. Sent: {self.is_sent}"
+#
+#
+# emails = []
+# line = input()
+#
+# while line != "Stop":
+#     line = line.split()
+#     email = Email(line[0], line[1], line[2])
+#     emails.append(email)
+#     line = input()
+#
+# send_emails = [int(n) for n in input().split(", ")]
+#
+# for index, email in enumerate(emails):
+#     if index in send_emails:
+#         emails[index].send()
+#     print(email.get_info())
+
+
+""" """
+
+
 class Email:
     def __init__(self, sender, receiver, content):
         self.sender = sender
@@ -45,18 +79,15 @@ class Email:
         return f"{self.sender} says to {self.receiver}: {self.content}. Sent: {self.is_sent}"
 
 
-emails = []
-line = input()
-
-while line != "Stop":
-    line = line.split()
-    email = Email(line[0], line[1], line[2])
-    emails.append(email)
-    line = input()
-
-send_emails = [int(n) for n in input().split(", ")]
-
-for index, email in enumerate(emails):
-    if index in send_emails:
-        emails[index].send()
-    print(email.get_info())
+email_object = []
+info = input()
+while info != "Stop":
+    sender, receiver, content = info.split(" ")
+    email_info = Email(sender, receiver, content)
+    email_object.append(email_info)
+    info = input()
+indexes = [int(index) for index in input().split(", ")]
+for index in indexes:
+    email_object[index].send()
+for current_email in email_object:
+    print(current_email.get_info())
