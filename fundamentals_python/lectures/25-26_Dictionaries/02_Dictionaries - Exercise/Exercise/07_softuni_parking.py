4701a63a019a6d18589b11584876fe30c63f9627
Nenogzar <stoyan.naidenov@gmail.com>
2024-03-04 11:06:49 +0200

    UPDATE

--- fundamentals_python/lectures/25-26_Dictionaries/02_Dictionaries - Exercise/Exercise/07_softuni_parking.py
+++ fundamentals_python/lectures/25-26_Dictionaries/02_Dictionaries - Exercise/Exercise/07_softuni_parking.py
@@ -1,100 +1,100 @@
-# class ParkingSystem:
-#     def __init__(self):
-#         self.users = {}  # Dictionary to store registered users and their license plates
-#
-#     def register_user(self, username, license_plate):
-#         if username in self.users:
-#             print(f"ERROR: already registered with plate number {self.users[username]}")
-#         else:
-#             self.users[username] = license_plate
-#             print(f"{username} registered {license_plate} successfully")
-#
-#     def unregister_user(self, username):
-#         if username not in self.users:
-#             print(f"ERROR: user {username} not found")
-#         else:
-#             print(f"{username} unregistered successfully")
-#             del self.users[username]
-#
-#     def print_registered_users(self):
-#         for username, license_plate in self.users.items():
-#             print(f"{username} => {license_plate}")
-#
-#
-# # Main program
-# if __name__ == "__main__":
-#     n = int(input())
-#     parking_system = ParkingSystem()
-#
-#     for _ in range(n):
-#         command_parts = input().split()
-#         command_type = command_parts[0]
-#
-#         if command_type == "register":
-#             _, username, license_plate = command_parts
-#             parking_system.register_user(username, license_plate)
-#         elif command_type == "unregister":
-#             _, username = command_parts
-#             parking_system.unregister_user(username)
-#
-#     parking_system.print_registered_users()
-####################
-# """  """
-#
-# dict_registered = {}
-#
-# reg_num = int(input())
-#
-# for _ in range(reg_num):
-#     info_string = input().split(" ")
-#
-#     if len(info_string) == 3:
-#         type_register, name, reg_num = [item for item in info_string]
-#
-#         if type_register == "register":
-#             if name not in dict_registered:
-#                 dict_registered[name] = reg_num
-#                 print(f"{name} registered {reg_num} successfully")
-#             else:
-#                 print(f"ERROR: already registered with plate number {dict_registered[name]}")
-#
-#     elif len(info_string) == 2:
-#         type_register, name = [item for item in info_string]
-#         if type_register == "unregister":
-#             if name in dict_registered:
-#                 del dict_registered[name]
-#                 print(f"{name} unregistered successfully")
-#             else:
-#                 print(f"ERROR: user {name} not found")
-#
-# for name, license in dict_registered.items():
-#     print(f"{name} => {license}")
-#
-####################
-# """ I.Shopov """
-# parking = {}
-# n = int(input())
-# for i in range(n):
-#     command = input().split()
-#     action = command[0]
-#
-#     if action == "register":
-#         key = command[1]
-#         value = command[2]
-#         if key in parking.keys():
-#             print(f"ERROR: already registered with plate number {parking[key]}")
-#         else:
-#             parking[key] = value
-#             print(f"{key} registered {value} successfully")
-#     elif action == "unregister":
-#         key = command[1]
-#         if key in parking.keys():
-#             del parking[key]
-#             print(f"{key} unregistered successfully")
-#         else:
-#             print(f"ERROR: user {key} not found")
-# for key in parking.keys():
-#     print(f"{key} => {parking[key]}")
+class ParkingSystem:
+    def __init__(self):
+        self.users = {}  # Dictionary to store registered users and their license plates
+
+    def register_user(self, username, license_plate):
+        if username in self.users:
+            print(f"ERROR: already registered with plate number {self.users[username]}")
+        else:
+            self.users[username] = license_plate
+            print(f"{username} registered {license_plate} successfully")
+
+    def unregister_user(self, username):
+        if username not in self.users:
+            print(f"ERROR: user {username} not found")
+        else:
+            print(f"{username} unregistered successfully")
+            del self.users[username]
+
+    def print_registered_users(self):
+        for username, license_plate in self.users.items():
+            print(f"{username} => {license_plate}")
+
+
+# Main program
+if __name__ == "__main__":
+    n = int(input())
+    parking_system = ParkingSystem()
+
+    for _ in range(n):
+        command_parts = input().split()
+        command_type = command_parts[0]
+
+        if command_type == "register":
+            _, username, license_plate = command_parts
+            parking_system.register_user(username, license_plate)
+        elif command_type == "unregister":
+            _, username = command_parts
+            parking_system.unregister_user(username)
+
+    parking_system.print_registered_users()
+###################
+"""  """
+
+dict_registered = {}
+
+reg_num = int(input())
+
+for _ in range(reg_num):
+    info_string = input().split(" ")
+
+    if len(info_string) == 3:
+        type_register, name, reg_num = [item for item in info_string]
+
+        if type_register == "register":
+            if name not in dict_registered:
+                dict_registered[name] = reg_num
+                print(f"{name} registered {reg_num} successfully")
+            else:
+                print(f"ERROR: already registered with plate number {dict_registered[name]}")
+
+    elif len(info_string) == 2:
+        type_register, name = [item for item in info_string]
+        if type_register == "unregister":
+            if name in dict_registered:
+                del dict_registered[name]
+                print(f"{name} unregistered successfully")
+            else:
+                print(f"ERROR: user {name} not found")
+
+for name, license in dict_registered.items():
+    print(f"{name} => {license}")
+
+###################
+""" I.Shopov """
+parking = {}
+n = int(input())
+for i in range(n):
+    command = input().split()
+    action = command[0]
+
+    if action == "register":
+        key = command[1]
+        value = command[2]
+        if key in parking.keys():
+            print(f"ERROR: already registered with plate number {parking[key]}")
+        else:
+            parking[key] = value
+            print(f"{key} registered {value} successfully")
+    elif action == "unregister":
+        key = command[1]
+        if key in parking.keys():
+            del parking[key]
+            print(f"{key} unregistered successfully")
+        else:
+            print(f"ERROR: user {key} not found")
+for key in parking.keys():
+    print(f"{key} => {parking[key]}")
 
 ####################
 """kuchovalcho """
--- fundamentals_python/lectures/25-26_Dictionaries/02_Dictionaries - Exercise/Exercise/08_courses.py
+++ fundamentals_python/lectures/25-26_Dictionaries/02_Dictionaries - Exercise/Exercise/08_courses.py
@@ -1,26 +1,26 @@
-# dict_courses = {}
-#
-# courses_info = input()
-# while courses_info != "end":
-#     info = courses_info.split(" : ")
-#     if len(info) > 1:
-#
-#         course_name, student_name = [item for item in info]
-#         if course_name not in dict_courses:
-#             dict_courses[course_name] = [student_name]
-#         else:
-#             if student_name not in dict_courses[course_name]:
-#                 dict_courses[course_name].append(student_name)
-#
-#     courses_info = input()
-#
-# for course_name, registered_students in dict_courses.items():
-#     print(f"{course_name}: {len(registered_students)}")
-#     for student_name in registered_students:
-#         print(f"-- {student_name}")
+dict_courses = {}
+
+courses_info = input()
+while courses_info != "end":
+    info = courses_info.split(" : ")
+    if len(info) > 1:
+
+        course_name, student_name = [item for item in info]
+        if course_name not in dict_courses:
+            dict_courses[course_name] = [student_name]
+        else:
+            if student_name not in dict_courses[course_name]:
+                dict_courses[course_name].append(student_name)
+
+    courses_info = input()
+
+for course_name, registered_students in dict_courses.items():
+    print(f"{course_name}: {len(registered_students)}")
+    for student_name in registered_students:
+        print(f"-- {student_name}")
         
 ####################
-""" """
+"""whit get() method  """
 
 dict_courses = {}
 
@@ -29,7 +29,7 @@ while courses_info != "end":
     courses = courses_info.split(" : ")
     course_name, student_name = courses[0], courses[1]
 
-    dict_courses[course_name] = dict_courses.get(course_name,  {})
+    dict_courses[course_name] = dict_courses.get(course_name, {})
     dict_courses[course_name][student_name] = student_name
 
     courses_info = input()
