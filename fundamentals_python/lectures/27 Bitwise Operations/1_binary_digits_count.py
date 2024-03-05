3c54a3c13576671a73756594d01cb47b1030e850
Nenogzar <stoyan.naidenov@gmail.com>
2024-03-05 08:20:56 +0200

    UPDATE

---
+++ fundamentals_python/lectures/27 Bitwise Operations/1_binary_digits_count.py
@@ -0,0 +1,8 @@
+n = int(input())
+b = int(input())
+binary_representation = bin(n)[2:]
+# print(binary_representation)
+count_b_digit = binary_representation.count(str(b))
+binary_digit_str = "zeroes" if b == 0 else "ones"
+print(f"{n} -> {binary_representation}")
+print(f"We have {count_b_digit} {binary_digit_str}.")
