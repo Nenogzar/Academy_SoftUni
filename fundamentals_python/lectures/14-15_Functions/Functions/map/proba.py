e7f4fe036612088f62bcb5338581edf3a90c0423
Nenogzar <stoyan.naidenov@gmail.com>
2024-03-10 10:00:16 +0200

    class map

--- fundamentals_python/lectures/14-15_Functions/Functions/map/proba.py
+++ fundamentals_python/lectures/14-15_Functions/Functions/map/proba.py
@@ -3,18 +3,13 @@ from icecream import ic
 numbers = list(range(-5, 6))
 # numbers = [num for num in range(-25, 26)]
 
-
-def info_number(num):
+ic(numbers)
+def square_num(num):
     if num % 2 == 0:
         return num ** 2
     return num ** 3
 
-
-info_fun = map(info_number, numbers)
-
-print(list(info_fun))
-
-number = list(range(-5, 6))
+ic(list(map(square_num, numbers)))
 
 
 def comparing(num):
@@ -29,6 +24,47 @@ def comparing(num):
     return [positive, negativ]
 
 
-comparing_list = comparing(numbers)
+ic(comparing(numbers))
 
-print(comparing_list)
+
+def multi_number(number):
+    return number * number
+
+ic(list(map(multi_number,numbers)))
+
+
+
+
+def multi_number(numbers):
+    result = []
+    for i in range(len(numbers) - 1):
+        result.append(numbers[i] * numbers[i+1])
+        # ic(numbers[i], "*", numbers[i + 1])
+    return result
+
+ic(multi_number(numbers))
+
+
+""" str to int and str to float"""
+
+list_str = "1.2", "-3.5", "0", "5", "12.33", "14"
+
+def safe_int_conversion(value):
+    try:
+        return int(value)
+    except ValueError:
+        ic(f"Could not convert {value} to int.")
+        return None
+
+ic(list(filter(None, map(safe_int_conversion, list_str))))
+
+
+def safe_float_conversion(value):
+    try:
+        return float(value)
+    except ValueError:
+        ic(f"Could not convert {value} to int.")
+        return None
+float_list = []
+ic(list(filter(None, map(safe_float_conversion, list_str))))
+
