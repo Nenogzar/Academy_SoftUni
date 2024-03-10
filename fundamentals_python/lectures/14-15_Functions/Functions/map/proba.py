da709120e97003aad636c22580b2b1d5b4e1bb51
Nenogzar <stoyan.naidenov@gmail.com>
2024-03-10 10:13:31 +0200

    class map

--- fundamentals_python/lectures/14-15_Functions/Functions/map/proba.py
+++ fundamentals_python/lectures/14-15_Functions/Functions/map/proba.py
@@ -1,15 +1,22 @@
 from icecream import ic
 
 numbers = list(range(-5, 6))
-# numbers = [num for num in range(-25, 26)]
-
 ic(numbers)
+ic("################################")
+
+
 def square_num(num):
     if num % 2 == 0:
         return num ** 2
     return num ** 3
 
+
 ic(list(map(square_num, numbers)))
+ic("comprehension")
+square_num1 = lambda num: num ** 2 if num % 2 == 0 else num ** 3
+ic(list(map(square_num1, numbers)))
+
+ic("################################")
 
 
 def comparing(num):
@@ -26,28 +33,36 @@ def comparing(num):
 
 ic(comparing(numbers))
 
+ic("################################")
 
-def multi_number(number):
+
+def multi_numbers1(number):
     return number * number
 
-ic(list(map(multi_number,numbers)))
+
+ic(list(map(multi_numbers1, numbers)))
+ic("comprehension")
+ic([x * x for x in numbers])
+
+ic("################################")
 
 
-
-
-def multi_number(numbers):
+def multi_number2(numbers):
     result = []
     for i in range(len(numbers) - 1):
-        result.append(numbers[i] * numbers[i+1])
-        # ic(numbers[i], "*", numbers[i + 1])
+        result.append(numbers[i] * numbers[i + 1])
+        ic(numbers[i], "*", numbers[i + 1])
     return result
 
-ic(multi_number(numbers))
 
+ic(multi_number2(numbers))
+
+ic("################################")
 
 """ str to int and str to float"""
 
 list_str = "1.2", "-3.5", "0", "5", "12.33", "14"
+
 
 def safe_int_conversion(value):
     try:
@@ -55,6 +70,7 @@ def safe_int_conversion(value):
     except ValueError:
         ic(f"Could not convert {value} to int.")
         return None
+
 
 ic(list(filter(None, map(safe_int_conversion, list_str))))
 
@@ -65,6 +81,7 @@ def safe_float_conversion(value):
     except ValueError:
         ic(f"Could not convert {value} to int.")
         return None
+
+
 float_list = []
 ic(list(filter(None, map(safe_float_conversion, list_str))))
-
