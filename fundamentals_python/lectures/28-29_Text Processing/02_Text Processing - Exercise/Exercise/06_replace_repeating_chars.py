c4b2e92c642e86d921bf3b4a2bc330d6c4e28682
Nenogzar <stoyan.naidenov@gmail.com>
2024-03-10 23:44:11 +0200

    repair

--- fundamentals_python/lectures/28-29_Text Processing/02_Text Processing - Exercise/Exercise/06_replace_repeating_chars.py
+++ fundamentals_python/lectures/28-29_Text Processing/02_Text Processing - Exercise/Exercise/06_replace_repeating_chars.py
@@ -2,7 +2,8 @@
 Write a program that reads a string from the console and
 replaces any sequence of the same letters with a single corresponding letter.
 """
-from icecream import  ic
+from icecream import ic
+
 text = input()
 new_str = ""
 final_str = ""
@@ -17,10 +18,8 @@ print(final_str)
 """ CEO """
 main_string = input()
 
-
 for pos, letter in enumerate(range(len(main_string) - 1), 1):
     if main_string[letter] != main_string[pos]:
-        # ic(main_string[letter], main_string[pos])
         print(main_string[letter], end="")  #
 
 print(main_string[-1])
