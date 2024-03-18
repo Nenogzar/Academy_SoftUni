d1356522ca7a7d8902f3c967174fb061a812cd27
Nenogzar <stoyan.naidenov@gmail.com>
2024-03-19 00:40:25 +0200

    Final Exam

--- fundamentals_python/lectures/38 Regular Final Exam/01/01_password_reset.py
+++ fundamentals_python/lectures/38 Regular Final Exam/01/01_password_reset.py
@@ -1,8 +1,10 @@
 def take_odd(password):
     return ''.join(password[i] for i in range(len(password)) if i % 2 != 0)
 
+
 def cut_password(password, index, length):
-    return password[:index] + password[index+length:]
+    return password[:index] + password[index + length:]
+
 
 def substitute_password(password, substring, substitute):
     if substring in password:
@@ -64,3 +66,45 @@ if __name__ == "__main__":
 #     command = input()
 #
 # print(f"Your password is: {password}")
+
+
+"""  from CEO - Python - Academy SoftUni"""
+
+# main_string = input()
+# command = input()
+#
+#
+# def take_odd_chr(main_string):
+#     result = "".join(main_string[x] for x in range(len(main_string)) if x % 2 != 0)
+#     print(result)
+#     return result
+#
+#
+# def cut_string(index, length, main_string):
+#     result = main_string[:index] + main_string[index + length:]
+#     print(result)
+#     return result
+#
+#
+# def substitute_string(substring, substitute, main_string):
+#     if substring in main_string:
+#         result = main_string.replace(substring, substitute)
+#         print(result)
+#         return result
+#     print("Nothing to replace!")
+#     return main_string
+#
+#
+# while command != "Done":
+#     if "TakeOdd" in command:
+#         main_string = take_odd_chr(main_string)
+#         command = input()
+#         continue
+#     _, index_or_substring, lenght_or_substitute = [int(x) if x.isdigit() else x for x in command.split()]
+#     if "Cut" in command:
+#         main_string = cut_string(index_or_substring, lenght_or_substitute, main_string)
+#     elif "Substitute" in command:
+#         main_string = substitute_string(index_or_substring, lenght_or_substitute, main_string)
+#     command = input()
+#
+# print(f"Your password is: {main_string}")
