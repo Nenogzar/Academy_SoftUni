de2887c572d4d756fa1b3337ea892c1e0e81b291
StoyanNaidenov <103338442+Nenogzar@users.noreply.github.com>
2024-03-22 15:05:20 +0200

    Update 01_registration.py

--- fundamentals_python/lectures/38 Regular Final Exam/01/01_registration.py
+++ fundamentals_python/lectures/38 Regular Final Exam/01/01_registration.py
@@ -1,51 +1,44 @@
-from icecream import ic
-user = input()
-
+user_name = input()
 
 register = input()
 while register != "Registration":
     key = register.split(" ")
-    ic(key)
     command = key[0]
+
     if command == 'Letters':
-        pass
-        if key[1] == 'Lower':
-            letter = key[1].lower()
-        elif key[1] == 'Upper':
-            letter == key[1].upper()
-        print(letter)
+        do_it = key[1]
+        if do_it == 'Lower':
+            user_name = user_name.lower()
+        elif do_it == 'Upper':
+            user_name = user_name.upper()
+        print(user_name)
+
     elif command == 'Reverse':
-        pass
+        start_index, end_index = int(key[1]), int(key[2])
+        if 0 <= start_index < len(user_name) and 0 <= end_index < len(user_name):
+            reversed_string = user_name[start_index: end_index + 1][::-1]
+            print(reversed_string)
+        else:
+            print("Invalid indices.")
+
     elif command == 'Substring':
-        pass
+        given_substring = key[1]
+        if given_substring in user_name:
+            user_name = user_name.replace(given_substring, "")
+            print(user_name)
+        else:
+            print(f"The username {user_name} doesn't contain {given_substring}.")
+
     elif command == 'Replace':
-        pass
+        replace_char = key[1]
+        user_name = user_name.replace(replace_char, "-")
+        print(user_name)
+
     elif command == 'IsValid':
-        pass
-
-
-
-
+        char_to_check = key[1]
+        if char_to_check in user_name:
+            print("Valid username.")
+        else:
+            print(f"{char_to_check} must be contained in your username.")
 
     register = input()
-
-
-
-
-
-"""
-
-John
-Letters Lower
-Substring SA
-IsValid @
-Registration
-
-asdad
-
-ThisIsSoftUni
-Reverse 1 3
-Replace S
-Substring hi
-Registration
-"""
