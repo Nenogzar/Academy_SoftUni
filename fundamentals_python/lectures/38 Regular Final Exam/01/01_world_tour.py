a6e439c0d7843172eb1a5f955f18bd8794be6c12
Nenogzar <stoyan.naidenov@gmail.com>
2024-03-23 18:37:34 +0200

    01_world_tour

--- fundamentals_python/lectures/38 Regular Final Exam/01/01_world_tour.py
+++ fundamentals_python/lectures/38 Regular Final Exam/01/01_world_tour.py
@@ -16,3 +16,46 @@ while command != "Travel":
 
 print(f"Ready for world tour! Planned stops: {containing_string}")
 
+""" CEO """
+
+
+# travel_place = input()
+#
+#
+# def main():
+#     command = input()
+#     while command != "Travel":
+#         command = command.split(":")
+#         if "Add Stop" in command[0]:
+#             add(int(command[1]), command[2])
+#
+#         elif "Remove Stop" in command[0]:
+#             remove(int(command[1]), int(command[2]))
+#
+#         elif command[0] == "Switch":
+#             switch(command[1], command[2])
+#
+#         print(travel_place)
+#         command = input()
+#     print(f"Ready for world tour! Planned stops: {travel_place}")
+#
+#
+# def add(index, string):
+#     global travel_place
+#     if 0 <= index < len(travel_place):
+#         travel_place = f"{travel_place[:index]}{string}{travel_place[index:]}"
+#
+#
+# def remove(start_index, end_index):
+#     global travel_place
+#     if 0 <= start_index and end_index < len(travel_place):
+#         travel_place = f"{travel_place[:start_index]}{travel_place[end_index + 1:]}"   # ending_index is  + 1 to jump over the last letter of the word
+#
+#
+# def switch(old_string, new_string):
+#     global travel_place
+#     if old_string in travel_place:
+#         travel_place = travel_place.replace(old_string, new_string)
+#
+#
+# main()
