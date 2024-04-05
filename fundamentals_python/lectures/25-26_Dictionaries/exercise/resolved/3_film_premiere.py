1520528df58c6ec7e4be138a9e17225a5bffdc72
Nenogzar <stoyan.naidenov@gmail.com>
2024-04-06 02:36:58 +0300

    dictionary exercise

--- fundamentals_python/lectures/25-26_Dictionaries/exercise/resolved/3_film_premiere.py
+++ fundamentals_python/lectures/25-26_Dictionaries/exercise/resolved/3_film_premiere.py
@@ -48,7 +48,7 @@
 #
 # print(f"Your bill is {total:.2f} leva.")
 
-
+###############################################################
 """ Dictionary """
 
 movie_name = input()
@@ -56,21 +56,10 @@ movie_extra = input()
 movie_tickets = int(input())
 
 movie_info = {
-    "John Wick":
-        {"Drink": 12,
-         "Popcorn": 15,
-         "Menu": 19},
-    "Star Wars":
-        {"Drink": 18,
-         "Popcorn": 25,
-         "Menu": 30},
-    "Jumanji":
-        {"Drink": 9,
-         "Popcorn": 11,
-         "Menu": 14}
+    "John Wick": {"Drink": 12, "Popcorn": 15, "Menu": 19},
+    "Star Wars": {"Drink": 18, "Popcorn": 25, "Menu": 30},
+    "Jumanji": {"Drink": 9, "Popcorn": 11, "Menu": 14}
 }
-
-
 
 # for movie, extras in movie_info.items():
 #     for extra, price in extras.items():
@@ -80,8 +69,6 @@ movie_info = {
 if movie_name in movie_info and movie_extra in movie_info[movie_name]:
     price = movie_info[movie_name][movie_extra]
     total = price * movie_tickets
-else:
-    total = 0.00
 
 if movie_tickets >= 4 and movie_name == "Star Wars":
     total *= 0.70
@@ -90,3 +77,34 @@ elif movie_tickets == 2 and movie_name == "Jumanji":
     total *= 0.85
 
 print(f"Your bill is {total:.2f} leva.")
+
+###############################################################
+
+""" other dictionaries are """
+
+movie_name = input()
+movie_extra = input()
+movie_tickets = int(input())
+
+movie_info = {
+    "John Wick":[12,15,19],
+
+    "Star Wars":[18,25,30],
+
+    "Jumanji":[9,11,14]
+}
+if movie_name in movie_info:
+    if movie_extra == "Drink":
+        price = movie_info[movie_name][0] * movie_tickets
+    elif movie_extra == "Popcorn":
+        price = movie_info[movie_name][1] * movie_tickets
+    elif movie_extra == "Menu":
+        price = movie_info[movie_name][2] * movie_tickets
+
+if movie_tickets >= 4 and movie_name == "Star Wars":
+    price *= 0.70
+
+elif movie_tickets == 2 and movie_name == "Jumanji":
+    price *= 0.85
+
+print(f"Your bill is {price:.2f} leva.")
