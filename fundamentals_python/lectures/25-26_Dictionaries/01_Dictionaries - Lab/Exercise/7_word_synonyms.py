d295c05d75b77075833cdccf8a47881239b1fe58
Nenogzar <stoyan.naidenov@gmail.com>
2024-04-07 17:42:40 +0300

    dictionary exercise

--- fundamentals_python/lectures/25-26_Dictionaries/01_Dictionaries - Lab/Exercise/7_word_synonyms.py
+++ fundamentals_python/lectures/25-26_Dictionaries/01_Dictionaries - Lab/Exercise/7_word_synonyms.py
@@ -29,15 +29,32 @@
 
 """ """
 
+#
+# count_of_synonyms = int(input())
+# synonyms = dict()
+#
+# for item in range(count_of_synonyms):
+#     word, synonym = input(), input()
+#
+#     synonyms[word] = synonyms.get(word, [])
+#     synonyms[word].append(synonym)
+#
+# [print(f"{word} - {', '.join(synonyms[word])}") for word in synonyms]
+
+""" list """
 
 count_of_synonyms = int(input())
 synonyms = dict()
-
+word_list, synonym_list = [], []
 for item in range(count_of_synonyms):
-    word, synonym = input(), input()
+    word_list.append(input())
+    synonym_list.append(input())
 
-    synonyms[word] = synonyms.get(word, [])
-    synonyms[word].append(synonym)
+    for word in word_list:
+        if word not in synonym_list:
+            synonyms[word] = []
+    for syn in synonym_list:
+        synonyms[word].append(syn)
+
 
 [print(f"{word} - {', '.join(synonyms[word])}") for word in synonyms]
-
