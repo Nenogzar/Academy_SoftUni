65e6ee74a1586886c67537cd2818037048a6a7eb
StoyanNaidenov <103338442+Nenogzar@users.noreply.github.com>
2024-05-21 17:25:35 +0300

    Update 03_2x2_squares_int_matrix.py

--- advanced_python/lectures/03_multidimensional_lists/exercise_I/03_2x2_squares_int_matrix.py
+++ advanced_python/lectures/03_multidimensional_lists/exercise_I/03_2x2_squares_int_matrix.py
@@ -63,6 +63,22 @@ C C K P  C C K P  C C K P
 """
 
 """ 1 """
+rows, cols = [int(x) for x in input().split()]
+
+matrix = [input().split() for row in range(rows)]
+
+counted_squares = 0
+
+for row in range(rows - 1):
+    for col in range(cols - 1):
+        if matrix[row][col] == matrix[row][col + 1] \
+                == matrix[row + 1][col] == matrix[row + 1][col + 1]:
+            counted_squares += 1
+
+print(counted_squares)
+
+
+""" 2 """
 class SquaresInMatrix:
 
     def __init__(self):
