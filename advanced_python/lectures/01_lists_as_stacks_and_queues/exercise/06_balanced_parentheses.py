f7066157143dbbbbf46e0a188854a28a5b4e0a69
StoyanNaidenov <103338442+Nenogzar@users.noreply.github.com>
2024-05-21 13:07:51 +0300

    Update 06_balanced_parentheses.py

--- advanced_python/lectures/01_lists_as_stacks_and_queues/exercise/06_balanced_parentheses.py
+++ advanced_python/lectures/01_lists_as_stacks_and_queues/exercise/06_balanced_parentheses.py
@@ -1,3 +1,54 @@
+#################################### TASK CONDITION ############################
+"""
+                         6. Balanced Parentheses
+You will be given a sequence consisting of parentheses. 
+Your job is to determine whether the expression is balanced. 
+A sequence of parentheses is balanced if every opening 
+parenthesis has a corresponding closing parenthesis that occurs 
+after the former. There will be no interval symbols between 
+the parentheses. You will be given three types of parentheses: (), {}, and [].
+
+{[()]} - Parentheses are balanced.
+(){}[] - Parentheses are balanced.
+{[(])} - Parentheses are NOT balanced.
+Input
+•	On a single line, you will receive a sequence of parentheses.
+Output 
+•	For each test case, print on a new line "YES" if the parentheses are balanced. 
+•	Otherwise, print "NO"
+Constraints
+•	1 ≤ lens ≤ 1000, where the lens is the length of the sequence
+•	Each character of the sequence will be one of {, }, (, ), [, ]
+
+____________________________________________________________________________________________
+Example_01
+
+Input
+{[()]}	
+
+Output
+YES
+
+____________________________________________________________________________________________
+Example_02
+
+Input
+{[(])}	
+
+Output
+NO
+
+____________________________________________________________________________________________
+Example_02
+
+Input
+{{[[(())]]}}
+
+Output
+YES
+
+"""
+""" 1 """
 # def is_balanced(expression):
 #     stack = []
 #     opening_brackets = "([{"
@@ -21,7 +72,7 @@
 #
 # print(is_balanced(input()))
 
-""" """
+""" 2 """
 from collections import deque
 
 parentheses = deque(input())
@@ -40,7 +91,7 @@ while parentheses:
 else:
     print("YES")
 
-""" """
+""" 3 """
 
 # from collections import deque
 #
@@ -66,7 +117,7 @@ else:
 # else:
 #     print("NO")
 
-""" """
+""" 4 """
 # from collections import deque
 #
 # expression = deque(input())
@@ -91,7 +142,7 @@ else:
 # else:
 #     print("Yes")
 
-"""  Bi """
+""" 5 - Bi """
 from collections import deque
 
 parentheses = list(input())
@@ -112,22 +163,19 @@ else:
     print("YES")
 
 
-""" """
+""" 6 """
 
 input_string = input()
 stack = []
 balanced = True
 
 for char in input_string:
-    # Ако символът е отваряща скоба, добавям го към стека
     if char in "([{":
         stack.append(char)
     else:
-        # Проверявам дали няма отварящи скоби, и ако е, това означава неправилно разположение на затваряща скоба
         if not stack:
             balanced = False
             break
-        # Изваждам последната добавена отваряща скоба от стека и проверявам дали отговаря на съответната затваряща скоба
         last_opening = stack.pop()
         if (char == ")" and last_opening != "(") or \
            (char == "]" and last_opening != "[") or \
@@ -142,3 +190,48 @@ if balanced:
     print("YES")
 else:
     print("NO")
+
+""" 7 """
+
+from collections import deque
+
+
+class BalancedParentheses:
+
+    def __init__(self):
+        self.sequence = deque(input())
+        self.is_balanced = True
+        self.opening_brackets = []
+        self.balancing = {"(": ")", "[": "]", "{": "}"}
+        self.message = ''
+        self.main()
+
+    def check_brackets(self, some_bracket):
+        opening_bracket = self.opening_brackets.pop()
+        if self.balancing[opening_bracket] != some_bracket:
+            self.is_balanced = False
+
+    def check_for_balancing(self):
+        while self.sequence and self.is_balanced:
+            bracket = self.sequence.popleft()
+            if bracket in "{[(":
+                self.opening_brackets.append(bracket)
+            elif self.opening_brackets:
+                self.check_brackets(bracket)
+            else:
+                self.is_balanced = False
+
+
+    def prepare_result(self):
+        self.message = 'YES' if self.is_balanced else 'NO'
+
+    def main(self):
+        self.check_for_balancing()
+        self.prepare_result()
+
+    def __repr__(self):
+        return self.message
+
+
+if __name__ == "__main__":
+    print(BalancedParentheses())
