74c74a9bf58dfad6210bfd1787f5f34344f556cf
StoyanNaidenov <103338442+Nenogzar@users.noreply.github.com>
2024-05-21 12:54:40 +0300

    Update 01_reverse_numbers.py

--- advanced_python/lectures/01_lists_as_stacks_and_queues/exercise/01_reverse_numbers.py
+++ advanced_python/lectures/01_lists_as_stacks_and_queues/exercise/01_reverse_numbers.py
@@ -1,3 +1,30 @@
+#################################### TASK CONDITION ############################
+"""
+
+                       1.	Reverse Numbers
+Write a program that reads a string with N integers from the console, 
+separated by a single space, and reverses them using a stack. 
+Print the reversed integers on one line, separated by a single space.
+
+____________________________________________________________________________________________
+Example_01
+
+Input
+1 2 3 4 5	
+
+Output
+5 4 3 2 1
+
+____________________________________________________________________________________________
+Example_02
+
+Input
+1
+
+Output
+1
+
+"""
 """ reverse numbers """
 input_list = list(map(int, input().split()))
 new_list =[]
@@ -21,6 +48,21 @@ for _ in range(len(reversed_list)):
     new_list.append(reversed_list.pop())
 print(" ".join(new_list))
 
+"" qceka88 """
+class Reversing:
 
-# input:                output:
-# 1 2 3 4 5             5 4 3 2 1
+    def __init__(self):
+        self.initial_string = input().split()
+        self.reversed_string = []
+        self.reversing_string()
+
+    def reversing_string(self):
+        while self.initial_string:
+            self.reversed_string.append(self.initial_string.pop())
+
+    def __repr__(self):
+        return f'{" ".join(self.reversed_string)}'
+
+
+if __name__ == "__main__":
+    print(Reversing())
