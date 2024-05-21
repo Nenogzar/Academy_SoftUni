4c6a2c7e3a2cb6e156563ffa417c4bd3c8248273
Nenogzar <stoyan.naidenov@gmail.com>
2024-05-21 21:21:32 +0300

    update

--- advanced_python/lectures/03_multidimensional_lists/lab/05_primary_diagonal.py
+++ advanced_python/lectures/03_multidimensional_lists/lab/05_primary_diagonal.py
@@ -35,10 +35,25 @@ Output
 
 ##################################### variant 01 #####################################
 
-print(sum([int(input().split()[row]) for row in range(int(input()))]))
 
 
 ##################################### variant 02 #####################################
+n = int(input())
+
+matrix = []
+for _ in range(n):
+    row = [int(x) for x in input().split()]
+    matrix.append(row)
+
+primary_sum = 0
+for i in range(n):
+    primary_sum += matrix[i][i]
+
+print(primary_sum)
+
+
+
+##################################### variant 03 #####################################
 class PrimaryDiagonal:
 
     def __init__(self):
