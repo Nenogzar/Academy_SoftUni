c67ad4f7e5c65f08655efd7782b86bad09c1de1a
Nenogzar <stoyan.naidenov@gmail.com>
2024-05-22 08:51:56 +0300

    update Santa present

--- advanced_python/lectures/02_1_stacks_queues_tuples_and_sets/05_santa_present_factory.py
+++ advanced_python/lectures/02_1_stacks_queues_tuples_and_sets/05_santa_present_factory.py
@@ -103,6 +103,8 @@ Teddy bear: 1
 
 
 """
+# https://judge.softuni.org/Contests/Practice/Index/3159#4
+
     ##########: variant 1 :##########
 
 from collections import deque
@@ -164,6 +166,54 @@ for toy in sorted(created_toys):
         print(f"{toy}: {created_toys[toy]}")
 
 
+
+    ##########: variant 2 :##########
+
+    from collections import deque
+
+    materials = deque(map(int, input().split()))
+    magic = deque(map(int, input().split()))
+
+    presents = {
+        "Doll": 150,
+        "Wooden train": 250,
+        "Teddy bear": 300,
+        "Bicycle": 400,
+    }
+    crafted_presents = {}
+
+    while materials and magic:
+        last_material = materials.pop()
+        first_magic = magic.popleft()
+        product = last_material * first_magic
+
+        if product in presents.values():
+            present = next(p for p, v in presents.items() if v == product)
+            if present not in crafted_presents:
+                crafted_presents[present] = 0
+            crafted_presents[present] += 1
+        elif product < 0:
+            materials.append(first_magic + last_material)
+        elif product > 0:
+            materials.append(last_material + 15)
+        else:
+            if last_material != 0:
+                materials.append(last_material)
+            if first_magic != 0:
+                magic.appendleft(first_magic)
+
+    merry_christmas = ("Doll" in crafted_presents and "Wooden train" in crafted_presents) or \
+                      ("Teddy bear" in crafted_presents and "Bicycle" in crafted_presents)
+
+    print("The presents are crafted! Merry Christmas!" if merry_christmas else "No presents this Christmas!")
+
+    if materials:
+        print(f"Materials left: {', '.join(map(str, reversed(materials)))}")
+    if magic:
+        print(f"Magic left: {', '.join(map(str, magic))}")
+
+    for toy_name, amount in sorted(crafted_presents.items()):
+        print(f"{toy_name}: {amount}")
 
     ##########: variant 3 - Class :##########
 
