0831fdaca0b67b8ba6a50ff9045732820bdd8a93
StoyanNaidenov <103338442+Nenogzar@users.noreply.github.com>
2024-05-27 17:00:29 +0300

    Update and rename 5_operate to 5_operate.py

---
+++ advanced_python/lectures/04_functions_advanced/lab/5_operate.py
@@ -0,0 +1,38 @@
+#################################### TASK CONDITION ############################
+'''
+https://judge.softuni.org/Contests/Practice/Index/1838#4
+
+                      5.	Operate
+Write a function called operate that receives an operator 
+("+", "-", "*" or "/") as first argument and multiple numbers (integers) 
+as additional arguments (*args). The function should return the result of
+the operator applied to all the numbers. For more clarification, see the examples below. 
+Submit only your function in the Judge system.
+Note: Be careful when you have multiplication and division
+
+_______________________________________________
+Example_01
+
+Test Code	(no input data in this task)
+print(operate("+", 1, 2, 3))	
+
+Output
+6	
+
+Explanation
+1 + 2 + 3 = 6
+
+_______________________________________________
+Example_02
+
+Test Code	(no input data in this task)
+print(operate("*", 3, 4))	
+
+Output
+12	
+
+Explanation
+3 * 4 = 12
+
+
+'''
