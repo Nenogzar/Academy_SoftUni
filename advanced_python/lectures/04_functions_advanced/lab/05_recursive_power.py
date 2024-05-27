8e99e614efb0621122f58990f15daeafaf9c1462
StoyanNaidenov <103338442+Nenogzar@users.noreply.github.com>
2024-05-27 17:07:42 +0300

    Create 05_recursive_power.py

---
+++ advanced_python/lectures/04_functions_advanced/lab/05_recursive_power.py
@@ -0,0 +1,30 @@
+#################################### TASK CONDITION ############################
+'''
+
+https://judge.softuni.org/Contests/Practice/Index/1838#5
+
+                   5.	Recursive Power
+Create a recursive function called recursive_power() which should receive a 
+number and a power. Using recursion, return the result of number ** power. 
+Submit only the function in the judge system.
+
+_______________________________________________
+Example_01
+
+Test Code	(no input data in this task)
+print(recursive_power(2, 10))	
+
+Output
+1024
+
+_______________________________________________
+Example_02
+
+Test Code	(no input data in this task)
+print(recursive_power(10, 100))	
+
+Output
+10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+
+'''
+##########: variant 1 :##########
