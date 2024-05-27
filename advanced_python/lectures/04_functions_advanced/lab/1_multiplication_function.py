1248a84c91372361485ff2c892b05a6c6626c424
StoyanNaidenov <103338442+Nenogzar@users.noreply.github.com>
2024-05-27 16:55:41 +0300

    Update 1_multiplication_function.py

---
+++ advanced_python/lectures/04_functions_advanced/lab/1_multiplication_function.py
@@ -0,0 +1,23 @@
+#################################### TASK CONDITION ############################
+'''
+https://judge.softuni.org/Contests/Practice/Index/1838#1
+
+                1.	Multiplication Function
+Write a function called multiply that can receive any quantity of numbers 
+(integers) as different parameters and returns the result of the 
+multiplication of all of them. Submit only your function in the Judge system.
+
+_______________________________________________
+Examples
+
+Test Code	(no input data in this task)
+print(multiply(1, 4, 5))
+print(multiply(4, 5, 6, 1, 3))
+print(multiply(2, 0, 1000, 5000))	
+
+Output
+20
+360
+0
+
+'''
