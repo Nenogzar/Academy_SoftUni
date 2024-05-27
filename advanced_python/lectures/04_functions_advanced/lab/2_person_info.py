6f8bfb8d63178c2a68ac49c24b21f47d1c1e2893
StoyanNaidenov <103338442+Nenogzar@users.noreply.github.com>
2024-05-27 16:56:23 +0300

    Update 2_person_info.py

--- advanced_python/lectures/04_functions_advanced/lab/2_person_info.py
+++ advanced_python/lectures/04_functions_advanced/lab/2_person_info.py
@@ -1,1 +1,24 @@
 
+#################################### TASK CONDITION ############################
+'''
+https://judge.softuni.org/Contests/Practice/Index/1838#1
+
+                        2.	Person Info
+Write a function called get_info that receives a name, an age, and a town 
+and returns a string in the format:
+
+"This is {name} from {town} and he is {age} years old".
+
+Use dictionary unpacking when testing your function. 
+Submit only the function in the judge system.
+
+_______________________________________________
+Example
+
+Test Code	(no input data in this task)
+print(get_info(**{"name": "George", "town": "Sofia", "age": 20}))	
+
+Output
+This is George from Sofia and he is 20 years old
+
+'''
