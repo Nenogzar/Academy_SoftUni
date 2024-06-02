0d2600e3f3609676a23d2b25684723e44a417ed4
StoyanNaidenov <103338442+Nenogzar@users.noreply.github.com>
2024-06-02 17:58:44 +0300

    Update auto_title.py

--- advanced_python/Exam/01_list_as_stack_and_queue/auto_title.py
+++ advanced_python/Exam/01_list_as_stack_and_queue/auto_title.py
@@ -3,7 +3,7 @@
 import os
 
 matrix = []
-input_string = input()
+input_string = input("File name: ")
 
 
 while input_string:
@@ -15,14 +15,15 @@ while input_string:
 
 for file_name in matrix:
     with open(f"{file_name}.py", "w") as file:
-        symbol  = "#"
-        file.write(f'{(30*symbol) + (" "+ file_name+ " ") + (30*symbol)}\n '
-                   f' {(30*symbol) + " TASK CONDITION " + (30*symbol)}'
-                   f'\n"""\n '
-                   f'{input("Judge link: ")}'
-                   f'\n"""\n'
-                   f'\n##########: variant 1 :##########\n\n\n'
-                   f'\n##########: variant 2 whit Dictionary :##########\n\n\n'
-                   f'\n##########: variant 3 solution SoftUni :##########\n\n\n')
+        symbol  = "# ******* "
+        file.write(f'{(symbol ) + input("Section : ") + ( symbol[::-1])}\n'
+                    f'\n{(symbol ) + (" "+ file_name + " ") + ( symbol[::-1])}\n '
+                    f'\n{(symbol ) + " TASK CONDITION " + ( symbol[::-1])}\n'
+                    f'"""\n '
+                    f'{input("Judge link: ")}\n'
+                    f'\n"""\n'
+                    f'\n##########: variant 1 :##########\n\n\n'
+                    f'\n##########: variant 2 :##########\n\n\n'
+                    f'\n##########: variant 3 solution SoftUni :##########\n\n\n')
 
-print("Файловете бяха успешно създадени.")
+print("The files were created successfully.")
