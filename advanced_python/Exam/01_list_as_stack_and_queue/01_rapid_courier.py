5897770ddd4e50983cbf3ed393c654f28519cc57
StoyanNaidenov <103338442+Nenogzar@users.noreply.github.com>
2024-06-24 12:18:53 +0300

    Update 01_rapid_courier.py

--- advanced_python/Exam/01_list_as_stack_and_queue/01_rapid_courier.py
+++ advanced_python/Exam/01_list_as_stack_and_queue/01_rapid_courier.py
@@ -184,6 +184,37 @@ elif not packages and couriers:
 
 ##########: variant 2 :##########
 
+from collections import deque
+
+packages = [int(x) for x in input().split()]
+couriers = deque(int(x) for x in input().split())
+total_weight = 0
+
+
+while packages and couriers:
+    package_weight = packages.pop()
+    courier_capacity = couriers.popleft()
+    total_weight += min(package_weight, courier_capacity)
+    weight_difference = courier_capacity - package_weight
+
+    if weight_difference < 0:
+        packages.append(package_weight - courier_capacity)
+
+    elif weight_difference - package_weight > 0:
+        couriers.append(weight_difference - package_weight)
+
+
+print(f"Total weight: {total_weight} kg")
+if not packages and not couriers:
+    print(f"Congratulations, all packages were delivered successfully by the couriers today.")
+
+elif packages and not couriers:
+    print(
+        f"Unfortunately, there are no more available couriers to deliver the following packages: {', '.join(str(x) for x in packages)}")
+
+elif couriers and not packages:
+    print(
+        f"TCouriers are still on duty: {', '.join(str(x) for x in couriers)} but there are no more packages to deliver.")
 
 
 ##########: variant 3 solution SoftUni :##########
