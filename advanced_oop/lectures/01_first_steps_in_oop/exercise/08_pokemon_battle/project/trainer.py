60e46a9401a638aef969dfa823d5b40318e30510
StoyanNaidenov <103338442+Nenogzar@users.noreply.github.com>
2024-07-04 12:44:26 +0300

    Update trainer.py

--- advanced_oop/lectures/01_first_steps_in_oop/exercise/08_pokemon_battle/project/trainer.py
+++ advanced_oop/lectures/01_first_steps_in_oop/exercise/08_pokemon_battle/project/trainer.py
@@ -15,6 +15,22 @@ class Trainer:
         return f"Caught {pokemon_obj.pokemon_details()}"
 
     def release_pokemon(self, pokemon_name):
+        # Option 1 -> fast but long
+        # pokemon = None
+        #
+        # for p in self.pokemons:
+        #     if p.name == pokemon_name:
+        #         pokemon = p
+        #         break
+
+        # Option 2 -> slow, but short
+        # try:
+        #     pokemon = [p for p in self.pokemons if p.name == pokemon_name][0]
+        # except IndexError:
+        #     return "Pokemon is not caught"
+
+        # Option 3 -> fast and short
+        
         try:
             pokemon = next(filter(lambda x: x.name == pokemon_name, self.pokemons))
             self.pokemons.remove(pokemon)
