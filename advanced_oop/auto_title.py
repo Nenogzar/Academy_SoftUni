e2bccadfd62519a8d38c7f44a246843f56cd1f3c
StoyanNaidenov <103338442+Nenogzar@users.noreply.github.com>
2024-07-05 16:52:16 +0300

    Update auto_title.py

--- advanced_oop/auto_title.py
+++ advanced_oop/auto_title.py
@@ -3,7 +3,9 @@
 import os
 
 matrix = []
-input_string = input()
+input_string = input("File name: ")
+
+
 while input_string:
     input_string = input_string.replace('*', '').replace('.', '').replace('-', '')
     input_string = ('_'.join(input_string.split())).lower()
@@ -11,10 +13,17 @@ while input_string:
     matrix.append(input_string)
     input_string = input()
 
-
 for file_name in matrix:
     with open(f"{file_name}.py", "w") as file:
+        symbol  = "# ******* "
+        file.write(f'{(symbol ) + input("Section : ") + ( symbol[::-1])}\n'
+                    f'\n{(symbol ) + (" "+ file_name + " ") + ( symbol[::-1])}\n '
+                    f'\n{(symbol ) + " TASK CONDITION " + ( symbol[::-1])}\n'
+                    f'"""\n '
+                    f'{input("Judge link: ")}\n'
+                    f'\n"""\n'
+                    f'\n##########: SOLUTION :##########\n\n\n'
+                    f'\n##########: TEST CODE :##########\n\n\n'
+                    f'\n##########: UNITTEST :##########\n\n\n')
 
-        file.write(f"# {file_name}\n")
-
-print("Файловете бяха успешно създадени.")
+print("The files were created successfully.")
