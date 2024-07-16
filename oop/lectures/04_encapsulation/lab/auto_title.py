c3c46ef3d9d116f27039bae5062c63d10fd8f0fb
StoyanNaidenov <103338442+Nenogzar@users.noreply.github.com>
2024-07-16 17:58:24 +0300

    Update auto_title.py

--- oop/lectures/04_encapsulation/lab/auto_title.py
+++ oop/lectures/04_encapsulation/lab/auto_title.py
@@ -2,9 +2,11 @@
 
 import os
 
+section_name = input("Section: ")
+judge_link = input("Judge link: ")
+
 matrix = []
 input_string = input("File name: ")
-
 
 while input_string:
     input_string = input_string.replace('*', '').replace('.', '').replace('-', '')
@@ -16,15 +18,15 @@ while input_string:
 for file_name in matrix:
     with open(f"{file_name}.py", "w") as file:
         symbol  = "# ******* "
-        file.write(f'{(symbol ) + input("Section : ") + ( symbol[::-1])}\n'
-                    f'\n{(symbol ) + (" "+ file_name + " ") + ( symbol[::-1])}\n '
-                    f'\n{(symbol ) + " TASK CONDITION " + ( symbol[::-1])}\n'
-                    f'"""\n '
-                    f'{input("Judge link: ")}\n'
-                    f'\n"""\n'
-                    f'\n##########: SOLUTION :##########\n\n\n'
-                    f'\n##########: TEST CODE :##########\n\n\n'
-                    f'\n"""\nOutput:\n\n """'
-                    f'\n##########: UNITTEST :##########\n\n\n')
+        file.write(f'{symbol}{section_name}{symbol[::-1]}\n'
+                   f'\n{symbol}{" " + file_name + " "}{symbol[::-1]}\n '
+                   f'\n{symbol} TASK CONDITION {symbol[::-1]}\n'
+                   f'"""\n'
+                   f'{judge_link}\n'
+                   f'\n"""\n'
+                   f'\n##########: SOLUTION :##########\n\n\n'
+                   f'\n##########: TEST CODE :##########\n\n\n'
+                   f'\n"""\nOutput:\n\n """'
+                   f'\n##########: UNITTEST :##########\n\n\n')
 
 print("The files were created successfully.")
