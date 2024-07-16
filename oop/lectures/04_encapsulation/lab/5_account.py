9009d3ef70c8c6ecefeb5d153e90a998278c537c
StoyanNaidenov <103338442+Nenogzar@users.noreply.github.com>
2024-07-16 23:40:19 +0300

    Update 5_account.py

--- oop/lectures/04_encapsulation/lab/5_account.py
+++ oop/lectures/04_encapsulation/lab/5_account.py
@@ -1,7 +1,7 @@
 # ******* Lab: Encapsulation ******* #
 
 # *******  5_account  ******* #
- 
+
 # *******  TASK CONDITION  ******* #
 """
  https://judge.softuni.org/Contests/1938/Encapsulation-Lab
@@ -10,14 +10,33 @@
 Create a class called Account. Upon initialization, it should receive an id, a balance, and a pin (all numbers).
     The pin and the id should be private instance attributes, and the balance should be a public attribute.
     Create two public instance methods:
-•	get_id(pin) - if the given pin is correct, return the id, otherwise, return "Wrong pin"
+
+•   get_id(pin) - if the given pin is correct, return the id, otherwise, return "Wrong pin"
+
 •	change_pin(old_pin, new_pin) - if the old pin is correct, change it to the new one and return
     "Pin changed", otherwise return "Wrong pin"
 
 """
 
-##########: SOLUTION :##########
 
+##########: SOLUTION :##########
+class Account:
+
+    def __init__(self, id: int, balance: int, pin: int):
+        self.__id = id
+        self.balance = balance
+        self.__pin = pin
+
+    def get_id(self, pin):
+        if pin == self.__pin:
+            return self.__id
+        return "Wrong pin"
+
+    def change_pin(self, old_pin, new_pin):
+        if old_pin == self.__pin:
+            self.__pin = new_pin
+            return f"Pin changed"
+        return "Wrong pin"
 
 
 ##########: TEST CODE :##########
@@ -27,8 +46,6 @@ print(account.get_id(3421))
 print(account.balance)
 print(account.change_pin(2212, 4321))
 print(account.change_pin(3421, 1234))
-
-
 
 """
 
@@ -41,5 +58,37 @@ Pin changed
 
  """
 ##########: UNITTEST :##########
+import unittest
 
 
+class Tests(unittest.TestCase):
+    def test_init(self):
+        a = Account(1234, 44, 4444)
+        self.assertEqual(a._Account__id, 1234)
+        self.assertEqual(a.balance, 44)
+        self.assertEqual(a._Account__pin, 4444)
+
+    def test_get_id_unsuccessfull(self):
+        a = Account(1234, 44, 4444)
+        res = a.get_id(1234)
+        self.assertEqual(res, "Wrong pin")
+
+    def test_get_id_successfull(self):
+        a = Account(1234, 44, 4444)
+        res = a.get_id(4444)
+        self.assertEqual(res, 1234)
+
+    def test_get_balance(self):
+        a = Account(1234, 44, 4444)
+        res = a.balance
+        self.assertEqual(res, 44)
+
+    def test_change_pin_successfull(self):
+        a = Account(1234, 44, 4444)
+        res = a.change_pin(4444, 1234)
+        self.assertEqual(res, "Pin changed")
+        self.assertEqual(a._Account__pin, 1234)
+
+
+if __name__ == "__main__":
+    unittest.main()
