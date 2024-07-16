eafeadcf4ee5e92b84f8a0bedc72b96a4b6ff35c
StoyanNaidenov <103338442+Nenogzar@users.noreply.github.com>
2024-07-16 22:51:51 +0300

    Update 4_email_validator.py

--- oop/lectures/04_encapsulation/lab/4_email_validator.py
+++ oop/lectures/04_encapsulation/lab/4_email_validator.py
@@ -1,13 +1,12 @@
 # ******* Lab: Encapsulation ******* #
 
 # *******  4_email_validator  ******* #
- 
+
 # *******  TASK CONDITION  ******* #
 """
  https://judge.softuni.org/Contests/1938/Encapsulation-Lab
 Create a class called EmailValidator. Upon initialization, it should receive:
-    •	min_length (of the username; example:
-        in "peter@gmail.com" "peter" is the username)
+    •	min_length (of the username; example:  in "peter@gmail.com" "peter" is the username)
     •	mails (list of the valid mails; example: "gmail", "abv")
     •	domains (list of valid domains; example: "com", "net")
 
@@ -23,10 +22,30 @@ Create one public method:
 •	validate(email) - using the three methods returns whether the email is valid (True/False)
 
 """
+from typing import List
+
 
 ##########: SOLUTION :##########
+class EmailValidator:
 
+    def __init__(self, min_length: int, mails: List[str], domains: List[str]):
+        self.min_length = min_length
+        self.mails = mails
+        self.domains = domains
 
+    def __is_name_valid(self, name):
+        return len(name) >= self.min_length
+
+    def __is_mail_valid(self, mail:str):
+        return mail in self.mails
+
+    def __is_domain_valid(self, domain):
+        return domain in self.domains
+
+    def validate(self, email):
+        name, main_data = email.split("@")
+        mail, domain = main_data.split(".")
+        return self.__is_name_valid(name) and self.__is_mail_valid(mail) and self.__is_domain_valid(domain)
 
 ##########: TEST CODE :##########
 
@@ -37,7 +56,6 @@ print(email_validator.validate("pe77er@gmail.com"))
 print(email_validator.validate("georgios@gmail.net"))
 print(email_validator.validate("stamatito@abv.net"))
 print(email_validator.validate("abv@softuni.bg"))
-
 
 """
 Output:
@@ -50,3 +68,38 @@ False
 ##########: UNITTEST :##########
 
 
+import unittest
+
+
+class Tests(unittest.TestCase):
+    def test_init(self):
+        ev = EmailValidator(5, ["me"], ["you", "he"])
+        self.assertEqual(ev.min_length, 5)
+        self.assertEqual(ev.mails, ["me"])
+        self.assertEqual(ev.domains, ["you", "he"])
+
+    def test_private_validate_name(self):
+        ev = EmailValidator(5, ["me"], ["you", "he"])
+        self.assertEqual(ev._EmailValidator__is_name_valid("abc"), False)
+        self.assertEqual(ev._EmailValidator__is_name_valid("abcdef"), True)
+
+    def test_private_validate_mail(self):
+        ev = EmailValidator(5, ["me"], ["you", "he"])
+        self.assertEqual(ev._EmailValidator__is_mail_valid("me"), True)
+        self.assertEqual(ev._EmailValidator__is_mail_valid("you"), False)
+
+    def test_private_validate_domain(self):
+        ev = EmailValidator(5, ["me"], ["you", "he"])
+        self.assertEqual(ev._EmailValidator__is_domain_valid("he"), True)
+        self.assertEqual(ev._EmailValidator__is_domain_valid("she"), False)
+
+    def test_validate(self):
+        ev = EmailValidator(5, ["me"], ["you", "he"])
+        self.assertEqual(ev.validate("itsme@me.you"), True)
+        self.assertEqual(ev.validate("me@me.you"), False)
+        self.assertEqual(ev.validate("itsme@me.she"), False)
+        self.assertEqual(ev.validate("itsme@you.he"), False)
+
+
+if __name__ == "__main__":
+    unittest.main()
