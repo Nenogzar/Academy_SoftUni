87377caaee70970018a4d4e079c7370a9a22394a
StoyanNaidenov <103338442+Nenogzar@users.noreply.github.com>
2024-07-18 16:36:45 +0300

    Update pizza.py

--- oop/lectures/04_encapsulation/exercise/02_pizza_maker/project/pizza.py
+++ oop/lectures/04_encapsulation/exercise/02_pizza_maker/project/pizza.py
@@ -1,39 +1,46 @@
 from project.dough import Dough
 from project.topping import Topping
 
+
 class Pizza:
 
     def __init__(self, name: str, dough: Dough, max_number_of_toppings: int):
-        if not name:
-            raise ValueError("The name cannot be an empty string")
-        self.__name = name
-
-        if dough is None:
-            raise ValueError("You should add dough to the pizza")
-        self.__dough = dough
-
-        if max_number_of_toppings <= 0:
-            raise ValueError("The maximum number of toppings cannot be less or equal to zero")
-        self.__max_number_of_toppings = max_number_of_toppings
-
-        self.__toppings = {}  # empty dictionary to store toppings
-
+        self.toppings: dict[Topping.topping_type, Topping.weight] = {}
+        self.name = name
+        self.dough = dough
+        self. max_number_of_toppings = max_number_of_toppings
 
     @property
     def name(self):
         return self.__name
 
+    @name.setter
+    def name(self, value):
+        if not value:
+            raise ValueError("The name cannot be an empty string")
+        self.__name = value
+
     @property
     def dough(self):
         return self.__dough
+
+    @dough.setter
+    def dough(self, value):
+        if value is None:
+            raise ValueError("You should add dough to the pizza")
+        self.__dough = value
 
     @property
     def max_number_of_toppings(self):
         return self.__max_number_of_toppings
 
-    @property
-    def toppings(self):
-        return self.__toppings
+    @max_number_of_toppings.setter
+    def max_number_of_toppings(self, value):
+        if value <= 0:
+            raise ValueError("The maximum number of toppings cannot be less or equal to zero")
+        self.__max_number_of_toppings = value
+
+
 
     def add_topping(self, topping: Topping):
         if self.max_number_of_toppings == len(self.toppings):
