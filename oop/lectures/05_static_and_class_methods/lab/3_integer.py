51a11bff02c0fa79a00a5c0b25e47a9505099726
StoyanNaidenov <103338442+Nenogzar@users.noreply.github.com>
2024-07-19 23:48:40 +0300

    Update 3_integer.py

--- oop/lectures/05_static_and_class_methods/lab/3_integer.py
+++ oop/lectures/05_static_and_class_methods/lab/3_integer.py
@@ -1,7 +1,7 @@
 # ******* Class and Static Methods ******* #
 
 # *******  3_integer  ******* #
- 
+
 # *******  TASK CONDITION  ******* #
 """
 https://judge.softuni.org/Contests/2430/Static-and-Class-Methods-Lab
@@ -24,11 +24,56 @@ Create a class called Integer.
 
 """
 
-##########: SOLUTION :##########
 
+##########: SOLUTION :##########
+class Integer:
+    def __init__(self, value: int):
+        self.value = value
+
+    @classmethod
+    def from_float(cls, float_value: float):
+        if not isinstance(float_value, float):
+            return "value is not a float"
+        return cls(int(float_value))
+
+    @classmethod
+    def from_roman(cls, value: str):
+        roman_numerals = {"I": 1,
+                          "V": 5,
+                          "X": 10,
+                          "L": 50,
+                          "C": 100,
+                          "D": 500,
+                          "M": 1000
+                          }
+
+        int_value = 0
+
+        for i in range(len(value)):
+            if value[i] in roman_numerals:
+                if (
+                        i + 1 < len(value)
+                        and roman_numerals[value[i]] < roman_numerals[value[i + 1]]
+                ):
+                    int_value -= roman_numerals[value[i]]
+                else:
+                    int_value += roman_numerals[value[i]]
+        return cls(int_value)
+
+
+    @classmethod
+    def from_string(cls, value: str):
+        if not isinstance(value, str):
+            return "wrong type"
+
+        try:
+            return cls(int(value))
+        except ValueError:
+            return "wrong type"
 
 
 ##########: TEST CODE :##########
+
 
 first_num = Integer(10)
 print(first_num.value)
@@ -38,7 +83,6 @@ print(second_num.value)
 
 print(Integer.from_float("2.6"))
 print(Integer.from_string(2.6))
-
 
 """
 Output:
@@ -50,4 +94,34 @@ wrong type
  """
 ##########: UNITTEST :##########
 
+import unittest
 
+
+class IntegerTests(unittest.TestCase):
+    def test_basic_init(self):
+        integer = Integer(1)
+        self.assertEqual(integer.value, 1)
+
+    def test_from_float_success(self):
+        integer = Integer.from_float(2.5)
+        self.assertEqual(integer.value, 2)
+
+    def test_from_float_wrong_type(self):
+        result = Integer.from_float("2.5")
+        self.assertEqual(result, "value is not a float")
+
+    def test_from_roman(self):
+        integer = Integer.from_roman("XIX")
+        self.assertEqual(integer.value, 19)
+
+    def test_from_string_success(self):
+        integer = Integer.from_string("10")
+        self.assertEqual(integer.value, 10)
+
+    def test_from_string_wrong_type(self):
+        result = Integer.from_string(1.5)
+        self.assertEqual(result, "wrong type")
+
+
+if __name__ == "__main__":
+    unittest.main()
