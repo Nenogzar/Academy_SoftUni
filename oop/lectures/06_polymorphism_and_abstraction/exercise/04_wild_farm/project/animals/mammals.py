68e99d07954f47935844efc83df855e0f9e43628
StoyanNaidenov <103338442+Nenogzar@users.noreply.github.com>
2024-07-24 12:12:46 +0300

    add solution of task

---
+++ oop/lectures/06_polymorphism_and_abstraction/exercise/04_wild_farm/project/animals/mammals.py
@@ -0,0 +1,86 @@
+from project.animals.animal import Mammal
+from project.food import Food, Vegetable, Fruit, Meat, Seed
+
+class Mouse(Mammal):
+    def __init__(self, name: str, weight: float, living_region: str):
+        super().__init__(name, weight, living_region)
+
+    def make_sound(self):
+        return "Squeak"
+
+    def eat(self, food: Food):
+        if isinstance(food, (Vegetable, Fruit)):
+            self.food_eaten += food.quantity
+            self.weight += food.quantity * 0.10
+        else:
+            print(f"{self.__class__.__name__} does not eat {food.__class__.__name__}!")
+
+    def feed(self, food: Food):
+        if isinstance(food, (Vegetable, Fruit)):
+            self.food_eaten += food.quantity
+            self.weight += food.quantity * 0.10
+        else:
+            return f"{self.__class__.__name__} does not eat {food.__class__.__name__}!"
+
+class Dog(Mammal):
+    def __init__(self, name: str, weight: float, living_region: str):
+        super().__init__(name, weight, living_region)
+
+    def make_sound(self):
+        return "Woof!"
+
+    def eat(self, food: Food):
+        if isinstance(food, Meat):
+            self.food_eaten += food.quantity
+            self.weight += food.quantity * 0.40
+        else:
+            print(f"{self.__class__.__name__} does not eat {food.__class__.__name__}!")
+
+    def feed(self, food: Food):
+        if isinstance(food, Meat):
+            self.food_eaten += food.quantity
+            self.weight += food.quantity * 0.40
+        else:
+            return f"{self.__class__.__name__} does not eat {food.__class__.__name__}!"
+
+class Cat(Mammal):
+    def __init__(self, name: str, weight: float, living_region: str):
+        super().__init__(name, weight, living_region)
+
+    def make_sound(self):
+        return "Meow"
+
+    def eat(self, food: Food):
+        if isinstance(food, (Vegetable, Meat)):
+            self.food_eaten += food.quantity
+            self.weight += food.quantity * 0.30
+        else:
+            print(f"{self.__class__.__name__} does not eat {food.__class__.__name__}!")
+
+    def feed(self, food: Food):
+        if isinstance(food, (Vegetable, Meat)):
+            self.food_eaten += food.quantity
+            self.weight += food.quantity * 0.30
+        else:
+            return f"{self.__class__.__name__} does not eat {food.__class__.__name__}!"
+
+class Tiger(Mammal):
+    def __init__(self, name: str, weight: float, living_region: str):
+        super().__init__(name, weight, living_region)
+
+    def make_sound(self):
+        return "ROAR!!!"
+
+    def eat(self, food: Food):
+        if isinstance(food, Meat):
+            self.food_eaten += food.quantity
+            self.weight += food.quantity * 1.00
+        else:
+            print(f"{self.__class__.__name__} does not eat {food.__class__.__name__}!")
+
+    def feed(self, food: Food):
+        if isinstance(food, Meat):
+            self.food_eaten += food.quantity
+            self.weight += food.quantity * 1.00
+        else:
+            return f"{self.__class__.__name__} does not eat {food.__class__.__name__}!"
