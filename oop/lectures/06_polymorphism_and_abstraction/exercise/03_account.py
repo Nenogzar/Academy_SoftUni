bb162d7040c60011cded4dcd7d4d2fe2003f5006
Nenogzar <stoyan.naidenov@gmail.com>
2024-07-24 18:40:33 +0300

    update

--- oop/lectures/06_polymorphism_and_abstraction/exercise/03_account.py
+++ oop/lectures/06_polymorphism_and_abstraction/exercise/03_account.py
@@ -51,28 +51,31 @@ Implement the correct magic methods so the code in the example below works prope
 
 
 ##########: SOLUTION :##########
+from typing import List
 
 class Account:
     def __init__(self, owner: str, amount=0):
         self.owner = owner
         self.amount = amount
-        self._transactions = []
+        self._transactions: List = []
+
+    @property
+    def balance(self):
+        return self.amount + sum(self._transactions)
 
     def handle_transaction(self, transaction_amount):
-        new_balance = self.balance + transaction_amount
-        if new_balance < 0:
+
+        if self.balance + transaction_amount < 0:
             raise ValueError("sorry cannot go in debt!")
         self._transactions.append(transaction_amount)
-        return f"New balance: {new_balance}"
+        return f"New balance: {self.balance}"
 
     def add_transaction(self, amount):
         if not isinstance(amount, int):
             raise ValueError("please use int for amount")
         return self.handle_transaction(amount)
 
-    @property
-    def balance(self):
-        return self.amount + sum(self._transactions)
+
 
     def __str__(self):
         return f"Account of {self.owner} with starting amount: {self.amount}"
