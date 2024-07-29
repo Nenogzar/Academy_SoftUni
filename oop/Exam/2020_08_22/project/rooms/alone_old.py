9f131e721e14cc0c9c7e8be6cb1f39fab3ab9151
StoyanNaidenov <103338442+Nenogzar@users.noreply.github.com>
2024-07-29 11:38:56 +0300

    Delete oop/Exam/2020_08_22 directory

--- oop/Exam/2020_08_22/project/rooms/alone_old.py
+++
@@ -1,9 +0,0 @@
-from project.rooms.room import Room
-
-
-class AloneOld(Room):
-
-    def __init__(self, family_name: str, pension: float):
-        self.member_in_room = 1
-        self.room_cost = 10
-        super().__init__(family_name, pension, self.member_in_room)
