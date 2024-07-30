d81e7cf37ac6caf7df567293e3388c93597c5dac
StoyanNaidenov <103338442+Nenogzar@users.noreply.github.com>
2024-07-30 13:12:32 +0300

    Delete oop/Exam/2021_04_10 directory

--- oop/Exam/2021_04_10/project/aquarium/freshwater_aquarium.py
+++
@@ -1,9 +0,0 @@
-from project.aquarium.base_aquarium import BaseAquarium
-
-
-class FreshwaterAquarium(BaseAquarium):
-    CAPACITY = 50
-
-    def __init__(self, name):
-        super().__init__(name, self.CAPACITY)
-        self.name = name
