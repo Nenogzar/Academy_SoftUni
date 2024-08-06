e26fbdfe191be552daaf1cf8c934ed4a8ab319c6
StoyanNaidenov <103338442+Nenogzar@users.noreply.github.com>
2024-08-07 00:03:16 +0300

    Delete oop/lectures/15_python_OOP_exam/2022_08_14 directory

--- oop/lectures/15_python_OOP_exam/2022_08_14/project/horse_specification/appaloosa.py
+++
@@ -1,6 +0,0 @@
-from project.horse_specification.horse import Horse
-class Appaloosa(Horse):
-    MAX_SPEED = 120
-    def __init__(self):
-
-
--- oop/lectures/15_python_OOP_exam/2022_08_14/project/probi.py
+++
@@ -1,7 +0,0 @@
-from project.jockey import Jockey
-
-try:
-    j = Jockey("Koko", 21)
-except ValueError as e:
-    print(e)
-
