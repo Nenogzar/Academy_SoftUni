2869ee1d006bfee9c0b549c96ec8126941982b51
StoyanNaidenov <103338442+Nenogzar@users.noreply.github.com>
2024-08-10 17:35:48 +0300

    add task and solution

---
+++ oop/lectures/15_python_OOP_exam/2024_08_10/project/valaidation/validation.py
@@ -0,0 +1,27 @@
+class Validation:
+    @staticmethod
+    def validate_len_white_space(value, length, message):
+        if len(value) < length:
+            raise ValueError(message)
+
+    @staticmethod
+    def validate_les_or_equal(value, les, message):
+        if value <= les:
+            raise ValueError(message)
+
+
+    @staticmethod
+    def valid_empty_white_space(value,message):
+        if value == "" or value.isspace():
+            raise ValueError(message)
+
+
+    @staticmethod
+    def three_white_space(value, message):
+        if len(value) != 3 or value.isspace():
+            raise ValueError(message)
+
+    @staticmethod
+    def validate_positive_number(value, message):
+        if value < 0:
+            raise ValueError(message)
