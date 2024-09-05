3858195fc44583f35e9148284dbde38a8d151148
Nenogzar <stoyan.naidenov@gmail.com>
2024-09-06 00:13:34 +0300

    update

--- basics_programming/test.py
+++ basics_programming/test.py
@@ -1,26 +1,27 @@
-NYLON_PRICE = 1.50
-PAINT_PRICE = 14.50
-THINNERS_PRICE = 5.00
-BAGS_PRICE = 0.40
-
-nylon = int(input())
-paint = int(input())
-thinners = int(input())
-work_hour_pay = int(input())
-
-nylon += 2
-paint += paint * 0.10
-
-price_materials = \
-    (nylon * NYLON_PRICE) + \
-    (paint * PAINT_PRICE) + \
-    (thinners * THINNERS_PRICE) + \
-    BAGS_PRICE
-
-work_price = price_materials * 0.30
-workers_pay = work_hour_pay * price_materials
-
-total_price = work_price + price_materials
-
-print(total_price)
+# NYLON_PRICE = 1.50
+# PAINT_PRICE = 14.50
+# THINNERS_PRICE = 5.00
+# BAGS_PRICE = 0.40
+#
+# nylon = int(input())
+# paint = int(input())
+# thinners = int(input())
+# work_hour_pay = int(input())
+#
+# nylon += 2
+# paint += paint * 0.10
+#
+# price_materials = \
+#     (nylon * NYLON_PRICE) + \
+#     (paint * PAINT_PRICE) + \
+#     (thinners * THINNERS_PRICE) + \
+#     BAGS_PRICE
+#
+# work_price = price_materials * 0.30
+# workers_pay = work_hour_pay * price_materials
+#
+# total_price = work_price + price_materials
+#
+# print(total_price)
+#
 
